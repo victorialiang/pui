b8ff7eb7fd467bbfe533ff8b6d5d395b9c7e53d0
Victoria Liang <victoria@Victorias-MacBook-Pro-2.local>
2018-10-01 11:49:27 -0400

    lab 6:

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -25,3 +25,35 @@ function completeListItem(item) {
         // in css, this would be: "text-decoration: none"
     }
 }
+
+$(document).ready(function(){
+    /*$("#add-item").click(function() { // bind handler for click event
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+      // append the input value within an li element
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+    });
+
+    $(".delete-item").click(function() {
+        console.log("test");
+        $(this).parent().remove();
+    });*/
+
+    $(document).on("click", ".delete-item", function() {
+        $(this).parent().remove();
+    });
+
+    $(document).on("click", "#add-item", function() {
+        var list = $("#grocery-list");
+        var itemInput = $("#new-list-item");
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+    });
+
+     $(document).on("click", ".complete-item", function() {
+        if $(this).checked
+        
+    });
+
+
+
+});
