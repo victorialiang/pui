7dbbda6563892b1b6c14c100b8a38e2705f282f3
Victoria Liang <victoril@andrew.cmu.edu>
2018-10-25 00:03:10 -0400

    colors change yay

--- homework_5/scripts.js
+++ homework_5/scripts.js
@@ -1,11 +1,58 @@
+/*** Global Variables **/
+
+var cart = [];
+
+
+/** Functions **/
+
+function Product(name, picture, color, cost, quantity) {
+	this.name = name;
+	this.picture = picture;
+	this.cost = cost;
+	this.quantity = quantity;
+	this.color = color;
+}
+
+
+function loadCart() {
+	//get cart
+
+}
+
+function saveCart() {
+
+}
+
+function addToCart(product) {
+	//call loadcart to get cart
+	//check if product is already in cart, append new product to cart
+	//call savecart
+
+
+}
+
+
+/*** Document Load ***/
+
 $(document).ready(function() {
  
   $('.color-choose input').on('click', function() {
-      var headphonesColor = $(this).attr('data-image');
+      var harnessColor = $(this).attr('data-image');
  
       $('.active').removeClass('active');
-      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $('.product-section img[data-image = ' + harnessColor + ']').addClass('active');
       $(this).addClass('active');
   });
+
+  // $("#button-storage").click(function() {
+  // 	var name = $(".productName")[0].innerText;
+  // 	var cost = $(".page-price")[0].innerText;
+  // 	var color = $(".color-choose input").attr('data-image');
+  // 	var picture = $("product-section img[data-image=" + color + ']');
+  // 	var quantity = 1;
+
+  // 	var product = new Product(name, picture, color, cost, quantity);
+  // 	addToCart(product);
+  // });
  
 });
